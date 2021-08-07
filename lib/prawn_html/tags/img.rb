025251701a45ad02464467c379bbc0a8d62ce0d3
Mattia Roccoberton <mat@blocknot.es>
2021-08-07 19:00:52 +0200

    img element

---
+++ lib/prawn_html/tags/img.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative 'base'
+
+module PrawnHtml
+  module Tags
+    class Img < Base
+      ELEMENTS = [:img].freeze
+
+      def block?
+        true
+      end
+
+      def custom_render(pdf, context)
+        styles = Attributes.parse_styles(attrs.hash.style)
+        context_options = context.merge_options
+        options = evaluate_styles(pdf, context_options.merge(styles))
+        pdf.image(@attrs.hash.src, options)
+      end
+
+      private
+
+      def evaluate_styles(pdf, styles)
+        options = {}
+        options[:width] = Attributes.convert_size(styles['width'], pdf.bounds.width) if styles.include?('width')
+        options[:height] = Attributes.convert_size(styles['height'], pdf.bounds.height) if styles.include?('height')
+        options[:position] = styles[:align] if %i[left center right].include?(styles[:align])
+        options
+      end
+    end
+  end
+end
