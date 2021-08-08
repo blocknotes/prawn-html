1d2d702cebe4f7dd11c230239930196d68f99e02
Mattia Roccoberton <mat@blocknot.es>
2021-08-08 08:22:16 +0200

    Add some examples

---
+++ examples/samples.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+$LOAD_PATH << '../lib'
+
+require 'prawn'
+require 'prawn-html'
+require 'oga'
+require 'pry'
+
+Dir[File.expand_path('*.html', __dir__)].sort.each do |file|
+  html = File.read(file)
+  pdf = Prawn::Document.new(page_size: 'A4', page_layout: :portrait)
+  PrawnHtml::HtmlHandler.new(pdf).process(html)
+  out = file.gsub(/\.html\Z/, '.pdf')
+  pdf.render_file(out)
+end
