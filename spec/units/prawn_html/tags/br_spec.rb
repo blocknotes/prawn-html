de81f02ed6de20f60683e11cc03e9dd1ed0b69ee
Mattia Roccoberton <mat@blocknot.es>
2021-09-03 12:28:07 +0200

    Fix Br tag spacing

--- spec/units/prawn_html/tags/br_spec.rb
+++ spec/units/prawn_html/tags/br_spec.rb
@@ -14,12 +14,21 @@ RSpec.describe PrawnHtml::Tags::Br do
   describe '#custom_render' do
     subject(:custom_render) { br.custom_render(pdf, context) }
 
-    let(:context) { instance_double(PrawnHtml::Context, last_text_node: false) }
+    let(:context) { instance_double(PrawnHtml::Context, last_text_node: false, previous_tag: nil) }
     let(:pdf) { instance_double(PrawnHtml::PdfWrapper, advance_cursor: true) }
 
-    it 'calls advance_cursor on the pdf wrapper' do
+    it "doesn't call advance_cursor on the pdf wrapper" do
       custom_render
-      expect(pdf).to have_received(:advance_cursor)
+      expect(pdf).not_to have_received(:advance_cursor)
+    end
+
+    context 'when the last node in the context is another br' do
+      let(:context) { instance_double(PrawnHtml::Context, last_text_node: false, previous_tag: :br) }
+
+      it 'calls advance_cursor on the pdf wrapper' do
+        custom_render
+        expect(pdf).to have_received(:advance_cursor)
+      end
     end
 
     context 'when the last node in the context is of type text' do
